dc96a6fdbbee864f226fd586641fc466bc86f500
Nizam <nizamuddin.ahmed@vantagecircle.com>
2021-12-17 11:32:54 +0530

    Added Readme

--- src/main/webapp/resources/assets/js/connection/socket.config.js
+++ src/main/webapp/resources/assets/js/connection/socket.config.js
@@ -1,7 +1,7 @@
 const SOCKET_CONN_OPEN = "socketOpen";
 const SOCKET_CONN_CLOSED = "socketClose"
 let socketStatus = "";
-const socketUrl = 'ws://' + window.location.host + '/stockrock/term';
+const socketUrl = 'ws://' + window.location.host + '/term';
 
 const socketConn = (onMessage) => {
     var ws = new WebSocket(socketUrl)
